d016a54ddb0e105b3bb0e384f0a208a4b96716a5
happy123guy <91419967+happy123guy@users.noreply.github.com>
2023-02-26 16:35:39 -0500

    Add files via upload

---
+++ livingRoom.js
@@ -0,0 +1,47 @@
+status = "";
+objects = [];
+img = "livingRoom.jpg";
+
+function preload(){
+    img = loadImage(img);
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.position(350, 200);
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    
+    if(status != ""){
+        for(i = 0; i<objects.length; i++){
+            fill("#0095ff");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("#0095ff");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+    document.getElementById("status").innerHTML = "There are " + objects.length + " objects in this image"
+    
+}
